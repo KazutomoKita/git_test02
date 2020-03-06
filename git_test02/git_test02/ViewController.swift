629f8a9a085390ec38ec831dd72639e74193a65d
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-03-06 12:41:47 +0900

    first commit

---
+++ git_test02/git_test02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git_test02
+//
+//  Created by Kazutomo Kita on 2020/03/06.
+//  Copyright © 2020 Kazutomo Kita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
